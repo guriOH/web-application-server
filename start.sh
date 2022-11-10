61f122517e9e4e2a516be19ef6ca1b61e48c94a4
henry.5 <henry.5@kakaoenterprise.com>
2022-11-10 17:51:58 +0900

    modify script

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
+mvn clean package
+
+
+pid=`ps -ef | grep java | grep 7070 | cut -d" " -f 5`
+
+echo $pid
+
+if [[ ! -z $pid ]]
+then
+  kill -9 $pid
+fi
 java -cp target/classes:target/dependency/* webserver.WebServer 7070 &
