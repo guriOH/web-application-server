9d9b7f05cd6ea5c65911c228a336375eb5d8f1e1
henry.5 <henry.5@kakaoenterprise.com>
2022-11-15 21:54:40 +0900

    요구사항 3,4

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,13 +31,21 @@ public class RequestHandler extends Thread {
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader rd = new BufferedReader(reader);
 
-            String res = readContents(rd);
-            String requestUrl = HttpRequestUtils.getUrl(res);
+            DataOutputStream dos = new DataOutputStream(out);
 
+            String contents = readContents(rd);
+            System.out.println(contents);
+            String requestUrl = HttpRequestUtils.getUrl(contents);
+
+
+
+
+            byte[] bytes = new byte[0];
             if(requestUrl.contains("?")) {
 
-                String url = requestUrl.substring(0, requestUrl.indexOf("?"));
-                String reqeustParams = requestUrl.substring(requestUrl.indexOf("?")+1);
+                int idx = requestUrl.indexOf("?");
+                String url = requestUrl.substring(0, idx);
+                String reqeustParams = requestUrl.substring(idx +1);
 
                 Map<String, String> paramMap = HttpRequestUtils.parseValues(reqeustParams, "&");
                 User user = new User(
@@ -45,13 +54,38 @@ public class RequestHandler extends Thread {
                         paramMap.get("name"),
                         paramMap.get("email")
                         );
-                System.out.println(user);
+            }else if(requestUrl.equals("/user/create")){
+                String body=null;
+                for (String line : contents.split("\n")) {
+                    if(line.contains("Content-Length:")){
+                        body = IOUtils.readData(rd,
+                                Integer.parseInt(HttpRequestUtils.parseHeader(line).getValue()));
+                    }
+
+                }
+                Map<String, String> paramMap = HttpRequestUtils.parseValues(body, "&");
+                User user = new User(
+                        paramMap.get("userId"),
+                        paramMap.get("password"),
+                        paramMap.get("name"),
+                        paramMap.get("email")
+                );
+                System.out.println(user.toString());
+                DataBase.addUser(user);
+
+                bytes = Files.readAllBytes(Paths.get("./webapp","/index.html"));
+
+                response302Header(dos, bytes.length,"/index.html");
+                // HTTP redirect
+                //301,302,303, 307 차이
+                // redirect, rewrite 차이
+
+            }else{
+                bytes = Files.readAllBytes(Paths.get("./webapp",requestUrl));
+                response200Header(dos, bytes.length);
             }
 
-            byte[]  bytes = Files.readAllBytes(Paths.get("./webapp",requestUrl));
 
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, bytes.length);
             responseBody(dos, bytes);
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -67,6 +101,7 @@ public class RequestHandler extends Thread {
 
             if(line == null) break;
         }
+
         return res;
     }
 
@@ -75,6 +110,18 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent, String redirectUrl) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Location: "+redirectUrl);
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
