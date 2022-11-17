6fadc90f495c812ff173a076e2d9124ee7e04a0a
henry.5 <henry.5@kakaoenterprise.com>
2022-11-17 18:05:44 +0900

    요구사항 5

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import db.DataBase;
 import model.User;
@@ -30,66 +31,92 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader rd = new BufferedReader(reader);
-
             DataOutputStream dos = new DataOutputStream(out);
+
 
             String contents = readContents(rd);
             System.out.println(contents);
             String requestUrl = HttpRequestUtils.getUrl(contents);
 
-
-
-
+            Map<String, String> cookies = getCookies(contents);
             byte[] bytes = new byte[0];
-            if(requestUrl.contains("?")) {
+            Map<String, String> paramMap = null;
+            if(requestUrl.equals("/user/create")){
+                String body = getRequestBody(rd, contents);
+                paramMap = HttpRequestUtils.parseValues(body, "&");
 
-                int idx = requestUrl.indexOf("?");
-                String url = requestUrl.substring(0, idx);
-                String reqeustParams = requestUrl.substring(idx +1);
+                // User create
+                User user = makeUser(paramMap);
 
-                Map<String, String> paramMap = HttpRequestUtils.parseValues(reqeustParams, "&");
-                User user = new User(
-                        paramMap.get("userId"),
-                        paramMap.get("password"),
-                        paramMap.get("name"),
-                        paramMap.get("email")
-                        );
-            }else if(requestUrl.equals("/user/create")){
-                String body=null;
-                for (String line : contents.split("\n")) {
-                    if(line.contains("Content-Length:")){
-                        body = IOUtils.readData(rd,
-                                Integer.parseInt(HttpRequestUtils.parseHeader(line).getValue()));
-                    }
-
-                }
-                Map<String, String> paramMap = HttpRequestUtils.parseValues(body, "&");
-                User user = new User(
-                        paramMap.get("userId"),
-                        paramMap.get("password"),
-                        paramMap.get("name"),
-                        paramMap.get("email")
-                );
-                System.out.println(user.toString());
+                // Insert new user
                 DataBase.addUser(user);
 
+                // redirect to /index.html
                 bytes = Files.readAllBytes(Paths.get("./webapp","/index.html"));
 
-                response302Header(dos, bytes.length,"/index.html");
                 // HTTP redirect
                 //301,302,303, 307 차이
                 // redirect, rewrite 차이
+                response302Header(dos, bytes.length,"/index.html");
+            }else if(requestUrl.equals("/user/login")){
+                String body = getRequestBody(rd, contents);
+                paramMap = HttpRequestUtils.parseValues(body, "&");
 
-            }else{
+                User user = DataBase.findUserById(paramMap.get("userId"));
+                if(Objects.isNull(user)){
+                    responseLogin(dos, bytes.length,"false","/user/login_failed.html");
+                }else{
+                    responseLogin(dos, bytes.length,"true","/index.html");
+                }
+            }else {
                 bytes = Files.readAllBytes(Paths.get("./webapp",requestUrl));
                 response200Header(dos, bytes.length);
             }
-
 
             responseBody(dos, bytes);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean runLogin() {
+        return false;
+    }
+
+    private String getRequestBody(BufferedReader rd, String contents) throws IOException {
+        String body=null;
+        for (String line : contents.split("\n")) {
+            if(line.contains("Content-Length:")){
+                body = IOUtils.readData(rd,
+                        Integer.parseInt(HttpRequestUtils.parseHeader(line).getValue()));
+            }
+
+        }
+        return body;
+    }
+
+    private Map<String, String> getCookies( String contents) throws IOException {
+        Map<String, String> cookies = new HashMap<String, String>();
+        for (String line : contents.split("\n")) {
+            if(line.contains("Cookie:")){
+                String[] tokens = line.split(":")[1].split(";");
+                for (String token : tokens){
+                    String[] split= token.split("=");
+                    cookies.put(split[0].trim(),split[1].trim());
+                }
+            }
+
+        }
+        return cookies;
+    }
+
+    private User makeUser(Map<String, String> paramMap) {
+        return new User(
+                paramMap.get("userId"),
+                paramMap.get("password"),
+                paramMap.get("name"),
+                paramMap.get("email")
+        );
     }
 
     private String readContents(BufferedReader rd) throws IOException {
@@ -103,6 +130,19 @@ public class RequestHandler extends Thread {
         }
 
         return res;
+    }
+
+    private void responseLogin(DataOutputStream dos, int lengthOfBodyContent, String result, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Location: "+url+"\r\n");
+            dos.writeBytes("Set-Cookie: logined="+result+";"+"Path=/\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,6 +3,8 @@ package webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,9 @@ public class WebServer {
         } else {
             port = Integer.parseInt(args[0]);
         }
+
+        User user = new User("1","1","1","1@com.com");
+        DataBase.addUser(user);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
 
